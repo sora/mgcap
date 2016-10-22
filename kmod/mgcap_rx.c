d9a3bf20c2d5cf2bb5d0ee54eabc736677a38e53
sora <sora@haeena.net>
2016-10-22 01:47:16 -0700

    kmod: refactoring

--- kmod/mgcap_rx.c
+++ kmod/mgcap_rx.c
@@ -11,19 +11,27 @@
 /* note: already called with rcu_read_lock */
 rx_handler_result_t mgc_handle_frame(struct sk_buff **pskb)
 {
+//	struct mgc_dev *dev = rcu_dereference(skb->dev->rx_handler_data);
 	rx_handler_result_t res = RX_HANDLER_CONSUMED;
 	struct sk_buff *skb = *pskb;
+	struct mgc_ring *rxbuf;
+	uint64_t tstamp;
+	uint16_t pktlen;
+	size_t copylen;
+	uint8_t *wrbuf;
 
-//	struct mgc_dev *dev = rcu_dereference(skb->dev->rx_handler_data);
-	struct mgc_ring *rxbuf = &mgc->rxrings[smp_processor_id()].buf;
+	// output buffer
+	rxbuf = &mgc->rxrings[smp_processor_id()].buf;
+	// input buffer
+	wrbuf = rxbuf->write;
 
-	struct skb_shared_hwtstamps *hwtstamps = skb_hwtstamps(skb);
-	uint16_t pktlen = skb->mac_len + skb->len;
-	size_t copylen = (pktlen > MGC_PKT_SNAPLEN) ? MGC_PKT_SNAPLEN : (size_t)pktlen;
+	tstamp = skb_hwtstamps(skb)->hwtstamp.tv64;
+	pktlen = skb->mac_len + skb->len;
+	copylen = (pktlen > MGC_PKT_SNAPLEN) ? MGC_PKT_SNAPLEN : (size_t)pktlen;
 
-	*(uint16_t *)rxbuf->write = pktlen;
-	*(uint64_t *)(rxbuf->write + MGC_HDR_PKTLEN_SIZE) = hwtstamps->hwtstamp.tv64;
-	memcpy((rxbuf->write + MGC_HDR_SIZE), skb_mac_header(skb), copylen);
+	*(uint16_t *)wrbuf = pktlen;
+	*(uint64_t *)(wrbuf + MGC_HDR_PKTLEN_SIZE) = tstamp;
+	memcpy((wrbuf + MGC_HDR_SIZE), skb_mac_header(skb), copylen);
 
 	ring_write_next(rxbuf, MGC_DATASLOT_SIZE);
 
